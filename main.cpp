e011395b9ba67d1af10c35b965376aaae8736df2
M. Scott O'Connor <michaeloconnor20@outlook.com>
2023-01-16 14:57:19 -0700

    added random value of new tile objects

--- main.cpp
+++ main.cpp
@@ -1,21 +1,52 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <ctime>
 
+using namespace std;
+
+class Tile {
+    public:
+    int value;
+    Tile()
+{    
+    int x = rand() % 100;
+    cout << x << endl;
+    if (x < 50)
+    {
+        value = 2;
+    }
+    else if (x < 90){
+        value = 4;
+    }
+    else{
+        value = 8;
+    }
+};
+    void doubleValue()
+{
+    value = value * 2;
+};
+};
 
 void displayBox() {
     std :: cout << " ___ ___ ___ ___ \n"
-         << "|   |   |   |   |\n"
-         << " ___ ___ ___ ___ \n"
-         << "|   |   |   |   |\n"
-         << " ___ ___ ___ ___ \n"
-         << "|   |   |   |   |\n"
-         << " ___ ___ ___ ___ \n"
-         << "|   |   |   |   |\n"
-         << " ___ ___ ___ ___ \n";
+                << "|   |   |   |   |\n"
+                << " ___ ___ ___ ___ \n"
+                << "|   |   |   |   |\n"
+                << " ___ ___ ___ ___ \n"
+                << "|   |   |   |   |\n"
+                << " ___ ___ ___ ___ \n"
+                << "|   |   |   |   |\n"
+                << " ___ ___ ___ ___ \n";
 }
 
 int main() {
+    srand(time(0));
     std :: cout << "Can you get to 2048?\n";
     displayBox();
+    Tile Bob;
+    std :: cout << Bob.value << "did it work?";
+
     return 0;
 }
